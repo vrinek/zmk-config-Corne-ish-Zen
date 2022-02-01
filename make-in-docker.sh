fb1d1452950547077ad9dc0cc0bfeccd11d07a2b
Kostas Karachalios <vrinek@hey.com>
2022-02-01 22:44:22 +0100

    Fix building the firmware
    It was not picking up the correct config

--- make-in-docker.sh
+++ make-in-docker.sh
@@ -8,8 +8,10 @@ west init -l config || true
 west update
 west zephyr-export
 
-west build --pristine -s zmk/app -b corne-ish_zen_left
+west build --pristine -s zmk/app -b corne-ish_zen_left \
+    -- -DZMK_CONFIG="/root/config/config"
 cp build/zephyr/zmk.uf2 firmware/corneish_zen_left.uf2
 
-west build --pristine -s zmk/app -b corne-ish_zen_right
+west build --pristine -s zmk/app -b corne-ish_zen_right \
+    -- -DZMK_CONFIG="/root/config/config"
 cp build/zephyr/zmk.uf2 firmware/corneish_zen_right.uf2
